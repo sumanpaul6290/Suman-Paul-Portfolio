4ecdded357874ca805359b6ab387711a6e04b3ff
sumanpaul6290 <paulsuman9903@gmail.com>
2025-03-03 22:52:05 +0530

    Update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* =========================================== typing animation ============================================== */
 var typed = new Typed(".typing",{
-    strings:["Web Designer", "Web Developer", "Graphic Designer", "YouTuber"],
+    strings:["Software Engineer", "Web Developer", "Data Science Intern"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
